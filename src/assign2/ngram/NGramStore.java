242c0c528ae27255775f663db07ff0563d427685
cynthia1991 <qiananger1234@gmail.com>
2014-05-23 19:39:20 +1000

    modified some problems

--- src/assign2/ngram/NGramStore.java
+++ src/assign2/ngram/NGramStore.java
@@ -17,11 +17,6 @@ import com.microsoft.research.webngram.service.GenerationService.TokenSet;
  */
 public class NGramStore implements NGramMap {
 
-	/*
-	 * public NGramMap{
-	 * 
-	 * }
-	 */
 	private List<NGramContainer> ngramContainersList = new ArrayList<NGramContainer>();
 	private static final String Key = "068cc746-31ff-4e41-ae83-a2d3712d3e68";
     private int MAXRESULTS = 5;
